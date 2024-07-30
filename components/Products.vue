99676e1f68ba4009aace8a70ffb5ae0cf35b6b2d
bartosz99 <62900319+bartosz99@users.noreply.github.com>
2024-07-30 12:02:58 +0200

    implement first version of products section

--- components/Products.vue
+++ components/Products.vue
@@ -1,3 +1,48 @@
+<script setup lang="ts">
+import product_1 from '../assets/images/products/product_1.png';
+import product_2 from '../assets/images/products/product_2.png';
+import product_3 from '../assets/images/products/product_3.png';
+import product_4 from '../assets/images/products/product_4.png';
+
+const products = [
+  {
+    id: 2,
+    name: 'Product 2',
+    image: product_2
+  },
+  {
+    id: 3,
+    name: 'Product 3',
+    image: product_3
+  },
+  {
+    id: 4,
+    name: 'Product 4',
+    image: product_4
+  }
+];
+
+const currentProduct = ref({
+  id: 1,
+  name: 'Product 1',
+  image: product_1
+});
+</script>
+
 <template>
-  <p>Products</p>
+  <div class="flex flex-col gap-4">
+    <div>
+      <img class="max-h-md max-w-md" :src="currentProduct.image" :alt="currentProduct.name" />
+    </div>
+    <div class="flex justify-between">
+      <div
+        class="max-w-32"
+        v-for="product in products"
+        :key="product.id"
+        @click="currentProduct = product"
+      >
+        <img :src="product.image" :alt="product.name" />
+      </div>
+    </div>
+  </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="flex">
-    <Products />
-    <div>
-      <h1 class="text-52 text-dark font-extrabold">Chance to win a StockX Mystery Box today!</h1>
-      <p class="text-22 text-dark">Unleash the Excitement of Mystery Electronics</p>
-      <Form />
+  <div class="p-8">
+    <div class="flex items-center gap-16">
+      <Products />
+      <div>
+        <h1 class="text-52 text-dark font-extrabold">Chance to win a StockX Mystery Box today!</h1>
+        <p class="text-22 text-dark">Unleash the Excitement of Mystery Electronics</p>
+        <Form />
+      </div>
     </div>
   </div>
 </template>
