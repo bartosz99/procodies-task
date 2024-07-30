56da744231fc9070af256ad26554a02441a9dd88
bartosz99 <62900319+bartosz99@users.noreply.github.com>
2024-07-30 12:33:07 +0200

    add features section

---
+++ constants/features.ts
@@ -0,0 +1,21 @@
+import feature_1 from '../assets/images/features/feature_1.png';
+import feature_2 from '../assets/images/features/feature_2.png';
+
+export const features = [
+  {
+    title: 'Curious about the StockX Mystery Box?',
+    description: [
+      'Experience the thrill of unboxing cutting-edge gadgets with Stockx Mystery Boxes. Each box is a treasure trove of high-quality electronics, handpicked to elevate your tech game.',
+      'Expect the unexpected! Our boxes are packed with the latest gadgets, from smartphones to gaming consoles, ensuring you get the best tech surprises every time.'
+    ],
+    image: feature_1
+  },
+  {
+    title: 'Why is everyone buzzing about these boxes?',
+    description: [
+      'StockX Mystery Boxes are not just about products; they’re about the experience. Imagine the excitement of unveiling top-tier electronics at a fraction of the cost',
+      'Our boxes include a variety of premium tech items, making it a must-have for every tech enthusiast. Join the buzz and see why everyone is raving about the unbeatable value and surprise factor of Stockx Mystery Boxes!'
+    ],
+    image: feature_2
+  }
+];
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import { features } from '../constants/index';
+</script>
+
 <template>
   <div class="p-8">
     <div class="flex items-center gap-16">
@@ -6,6 +10,30 @@
         <h1 class="text-52 text-dark font-extrabold">Chance to win a StockX Mystery Box today!</h1>
         <p class="text-22 text-dark">Unleash the Excitement of Mystery Electronics</p>
         <Form />
+      </div>
+    </div>
+    <div class="w-full flex flex-col items-center my-16">
+      <h2 class="text-48 font-bold text-green2 text-center">
+        Top Tech Mystery Box <br />
+        at Unbeatable Prices
+      </h2>
+      <p class="text-20 font-medium text-green">
+        Fast shipping | Easy returns | Special discounts for students
+      </p>
+    </div>
+    <div class="w-full flex flex-col items-center my-16">
+      <div
+        class="flex gap-32 items-center"
+        :class="{ 'flex-row-reverse': index % 2 !== 0 }"
+        v-for="(feature, index) in features"
+      >
+        <img :src="feature.image" :alt="feature.title" />
+        <div>
+          <h3 class="text-42 mb-6 font-bold color-dark">{{ feature.title }}</h3>
+          <p class="font-normal leading-8 mb-3" v-for="paragraph in feature.description">
+            {{ paragraph }}
+          </p>
+        </div>
       </div>
     </div>
   </div>
