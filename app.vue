b2a3197a8260e4d9a27227cde0cde881f21df3df
bartosz99 <62900319+bartosz99@users.noreply.github.com>
2024-07-30 08:43:20 +0200

    remove nuxt boilerplate

--- app.vue
+++ app.vue
@@ -1,6 +1,24 @@
+<script setup lang="ts">
+useHead({
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+  ],
+  link: [{ rel: 'icon', href: '/favicon.svg' }],
+  htmlAttrs: {
+    lang: 'en'
+  }
+});
+
+useSeoMeta({
+  titleTemplate: 'Chance to win a StockX Mystery Box today!'
+});
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
