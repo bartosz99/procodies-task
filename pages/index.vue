e01cc2ce29cc0efcf99faa1d41b2ed5dbec7b0fe
bartosz99 <62900319+bartosz99@users.noreply.github.com>
2024-07-30 09:30:21 +0200

    implement tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,3 @@
-<template></template>
+<template>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif']
+    },
+    colors: {
+      dark: '#293632',
+      green: '#006340'
+    }
+  }
+};
